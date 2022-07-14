7cbb3394538540110fa07beb0fb1e855878b1916
Pavlo-levitskiy <90378679+Pavlo-levitskiy@users.noreply.github.com>
2022-07-14 16:45:06 +0300

    correction

--- src/components/Button/Button.styled.js
+++ src/components/Button/Button.styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const ButtonLoad = styled.button`
   padding: 8px 16px;
-  border-radius: 2px;
+  border-radius: 8px;
   background-color: #3f51b5;
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   text-align: center;
@@ -16,7 +16,9 @@ export const ButtonLoad = styled.button`
   line-height: 24px;
   font-style: normal;
   font-weight: 500;
-  min-width: 180px;
+  width: 180px;
+  margin-left: auto;
+  margin-right: auto;
   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
   &:hover,
